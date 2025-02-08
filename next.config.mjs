9d39096e60ba6272c1ea442285d05d2a7262c7b4
areeba-binteali <areeba42101@gmail.com>
2025-02-08 13:45:40 +0500

    Initial Commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
       domains: ['cdn.sanity.io'],
       unoptimized: true,
     },
+    async rewrites() {
+      return [
+        {
+          source: "/:path*",
+          destination: "/public/:path*",
+        },
+      ];
+    },
   };
   
   export default nextConfig;
